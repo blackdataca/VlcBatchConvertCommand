761a3ea594a7408893c0d1006f757d0556a78151
Robin Clarke <25224498+1225g@users.noreply.github.com>
2023-05-24 07:31:37 -0700

    Create convert.ps1

---
+++ convert.ps1
@@ -0,0 +1,26 @@
+foreach($inputFile in get-childitem -recurse -Filter *.mts)
+{
+	$outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + ".mp4";
+	$outputFileName = [System.IO.Path]::Combine($inputFile.DirectoryName, $outputFileName);
+
+	Write-Host $inputFile " " -NoNewline
+	if ([System.IO.File]::Exists($outputFileName))
+	{
+		 Write-Host "skip" 
+	}
+	else
+	{
+		Write-Host "converting..." -NoNewline
+		$programFiles = ${env:ProgramFiles};
+
+		if($programFiles -eq $null) { $programFiles = $env:ProgramFiles; }
+
+		$processName = $programFiles + "\VideoLAN\VLC\vlc.exe"
+		
+		$processArgs = "-I dummy -vvv `"$($inputFile.FullName)`" --sout=#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100,vcodec=h264}:standard{access=file,mux=mp4,dst=`"$outputFileName`"} vlc://quit"
+
+		start-process $processName $processArgs -wait
+		Write-Host "done"
+	}
+
+}
