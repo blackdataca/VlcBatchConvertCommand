f965eaf9a048b3eea0cbd3f510f15136a32754f5
Robin Clarke <25224498+1225g@users.noreply.github.com>
2023-05-24 10:19:21 -0700

    Update convert.ps1

--- convert.ps1
+++ convert.ps1
@@ -1,5 +1,5 @@
 $inputFilter = "*.mts"
-Write-Host "VLC Batch Convert to mp4 by BlackDataCa 2023 To conver all files, change value of $inputFilter to *.*"
+Write-Host "VLC Batch Convert to mp4 by BlackData 2023 To conver all files, change $inputFilter to *.*"
 foreach($inputFile in get-childitem -recurse -Filter $inputFilter)
 {
 	$outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + ".mp4";
@@ -16,7 +16,10 @@ foreach($inputFile in get-childitem -recurse -Filter $inputFilter)
 
 		$processName = ${env:ProgramFiles} + "\VideoLAN\VLC\vlc.exe"
 		
-		$processArgs = "-I dummy -vvv `"$($inputFile.FullName)`" --sout=#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100,vcodec=h264}:standard{access=file,mux=mp4,dst=`"$outputFileName`"} vlc://quit"
+		#Uncomment to hide UI 
+		#$processArgs = "-I dummy"
+		
+		$processArgs = $processArgs + " -vv `"$($inputFile.FullName)`" --sout=#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100,vcodec=h264}:standard{access=file,mux=mp4,dst=`"$outputFileName`"} --rc-show-pos vlc://quit"
 
 		start-process $processName $processArgs -wait
 		Write-Host "done"
